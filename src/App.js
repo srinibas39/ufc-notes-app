ee73c9a6afde82ae757f596d924406eb11115ef8
srinibas <srinibaskhuntia39@gmail.com>
2022-06-11 13:20:06 +0530

    editor component done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
 
+
 import { Hero } from "./components/Hero/Hero";
-
-
-
-
 
 export const App = () => {
   return <>
     <Hero />
-    
   </>
 }
 
---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,39 @@
+
+import "./Editor.css"
+export const Editor = () => {
+    return <div className="editor">
+        <div className="editor-header">
+            <input type="text" placeholder="Type Title of the Note" />
+            <span class="material-symbols-outlined">
+                push_pin
+            </span>
+        </div>
+        <div className="editor-body">
+            <textarea placeholder="Type Body of the Text"></textarea>
+        </div>
+        <div className="editor-tags">
+            <button>tag 1</button>
+            <button>tag 1</button>
+            <button>tag 1</button>
+        </div>
+        <div className="editor-footer">
+            <small>6/11/2022, 12:04:50 PM</small>
+            <select>
+                <option>Low Priority</option>
+                <option>Medium Priority </option>
+                <option>High Priority</option>
+            </select>
+            <div className="editor-option">
+                <span class="material-symbols-outlined">
+                    check_circle
+                </span>
+                <span class="material-symbols-outlined">
+                    palette
+                </span>
+                <span class="material-symbols-outlined">
+                    label
+                </span>
+            </div>
+        </div>
+    </div>
+}
