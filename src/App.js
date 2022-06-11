5ea9fbc1407d8b684b9db6914fb23e8f3e372708
srinibas <srinibaskhuntia39@gmail.com>
2022-06-11 09:45:11 +0530

    note Ui done

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import "./App.css";
 import { Hero } from "./components/Hero/Hero";
 
 
+
 export const App = () => {
   return <>
-   <Hero/>
+    <Hero/>
+  
   </>
 }
 
---
+++ src/components/Note/Note.js
@@ -0,0 +1,40 @@
+import "./Note.css"
+export const Note = () => {
+    return <div className="note">
+        <div className="note-header">
+            <h3>Title of the Note</h3>
+            <span class="material-symbols-outlined">
+                push_pin
+            </span>
+        </div>
+        <div className="note-body">
+            Body of the Note
+        </div>
+        <div className="note-tags">
+            <button>tag1</button>
+            <button>tag2</button>
+            <button>tag2</button>
+        </div>
+        <div className="note-footer">
+            <small>Created on 26/10/2021</small>
+            <div className="note-options">
+                <span class="material-symbols-outlined">
+                    edit
+                </span>
+                <span class="material-symbols-outlined">
+                    palette
+                </span>
+                <span class="material-symbols-outlined">
+                    archive
+                </span>
+                <span class="material-symbols-outlined">
+                    delete
+                </span>
+                <span class="material-symbols-outlined">
+                    label
+                </span>
+            </div>
+        </div>
+
+    </div>
+}
