83f1252c23821fcd66d08de5ca3c4135717b2fbb
srinibas <srinibaskhuntia39@gmail.com>
2022-06-14 07:35:54 +0530

    populated archives

--- src/pages/ArchivePage.js
+++ src/pages/ArchivePage.js
@@ -1,15 +1,19 @@
 
-import { NavBar } from "../components/NavBar/NavBar"
-import { Note } from "../components/Note/Note"
+import { NavBar } from "../components/NavBar/NavBar";
+import { Note } from "../components/Note/Note";
+import { useSelector } from "react-redux";
 
 export const ArchivePage = () => {
+    const { archives } = useSelector((state) => state.notes);
     return <>
         <NavBar />
         <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
             <h2 className="primary-color">ARCHIVES</h2>
-            <Note />
-            <Note />
-            <Note />
+            {
+                archives ? archives.map((note) => {
+                    return <Note note={note} key={note._id} />
+                }) : ""
+            }
         </div>
     </>
 }
